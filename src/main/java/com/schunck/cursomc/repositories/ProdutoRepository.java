df6d4019fa31ea15239a14b35ffc87fb3d03fbf9
Marcelo Schunck <schunckbr@gmail.com>
2021-02-22 08:35:06 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/schunck/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package com.schunck.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.schunck.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+	
+}
